d0eba4fee0a658b35487d9c8fe92505ebb6787cf
facuCast123 <facundo022020@gmail.com>
2024-09-19 20:21:05 -0300

    Created floating can scene
    -Created 'FloatingCan' component.
    -Created new 'Scene' component in Hero slice.
    -Added scene to Hero index.

--- src/components/ViewCanvas.tsx
+++ src/components/ViewCanvas.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { SodaCan } from "./SodaCan";
-import { Environment } from "@react-three/drei";
+import { View } from "@react-three/drei";
 
 type Props = {};
 
@@ -25,8 +24,7 @@ export default function ViewCanvas({}: Props) {
         fov: 30,
       }}
     >
-      <SodaCan />
-      <Environment files="/hdr/lobby.hdr" environmentIntensity={1.5} />
+      <View.Port />
     </Canvas>
   );
 }
