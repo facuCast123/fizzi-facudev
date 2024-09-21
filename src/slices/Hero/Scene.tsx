ec6d220965fabbd024bc81de493cdebd749054bd
facuCast123 <facundo022020@gmail.com>
2024-09-21 18:54:19 -0300

    Completed Hero animation
    -Added a scroll timeline for animating the group of cans on scroll.
    -Added conditional to 'introTl' for consistent animation.

--- src/slices/Hero/Scene.tsx
+++ src/slices/Hero/Scene.tsx
@@ -5,10 +5,11 @@ import { Environment, OrbitControls } from "@react-three/drei";
 import { Group } from "three";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import FloatingCan from "@/components/FloatingCan";
 
-gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 type Props = {};
 
@@ -58,11 +59,55 @@ export default function Scene({}: Props) {
       },
     });
 
-    introTl
-      .from(can1GroupRef.current.position, { y: -5, x: 1 }, 0)
-      .from(can1GroupRef.current.rotation, { z: 3 }, 0)
-      .from(can2GroupRef.current.position, { y: 5, x: 1 }, 0)
-      .from(can2GroupRef.current.rotation, { z: 3 }, 0);
+    if (window.scrollY < 20) {
+      introTl
+        .from(can1GroupRef.current.position, { y: -5, x: 1 }, 0)
+        .from(can1GroupRef.current.rotation, { z: 3 }, 0)
+        .from(can2GroupRef.current.position, { y: 5, x: 1 }, 0)
+        .from(can2GroupRef.current.rotation, { z: 3 }, 0);
+    }
+
+    const scrollTl = gsap.timeline({
+      defaults: {
+        duration: 2,
+      },
+      scrollTrigger: {
+        trigger: "hero",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1.5,
+      },
+    });
+
+    scrollTl
+      // Rotate can group
+      .to(groupRef.current.rotation, { y: Math.PI * 2 })
+
+      // Can 1 - Strawberry lemonade
+      .to(can1Ref.current.position, { x: -0.2, y: -0.7, z: -2 }, 0)
+      .to(can1Ref.current.rotation, { z: 0.3 }, 0)
+
+      // Can 2 - Watermelon
+      .to(can2Ref.current.position, { x: 1, y: -0.2, z: -1 }, 0)
+      .to(can2Ref.current.rotation, { z: 0 }, 0)
+
+      // Can 3 - Black cherry
+      .to(can3Ref.current.position, { x: -0.3, y: 0.5, z: -1 }, 0)
+      .to(can3Ref.current.rotation, { z: -0.1 }, 0)
+
+      // Can 4 - Lemon lime
+      .to(can4Ref.current.position, { x: 0, y: -0.3, z: 0.5 }, 0)
+      .to(can4Ref.current.rotation, { z: 0.3 }, 0)
+
+      // Can 5 - Grape
+      .to(can5Ref.current.position, { x: 0.3, y: 0.5, z: -0.5 }, 0)
+      .to(can5Ref.current.rotation, { z: -0.25 }, 0)
+
+      .to(
+        groupRef.current.position,
+        { x: 1, duration: 3, ease: "sine.inOut" },
+        1.3,
+      );
   });
 
   return (
