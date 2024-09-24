c28d6f4f42abc6d746d7f4d7c0d339f044754419
facuCast123 <facundo022020@gmail.com>
2024-09-24 18:42:26 -0300

    Created SkyDive scene
    -Created 'Scene' component in 'SkyDive' slice.
    -Added FloatingCan and Clouds (missing text).

---
+++ src/slices/SkyDive/Scene.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import FloatingCan from "@/components/FloatingCan";
+import { Content } from "@prismicio/client";
+import { Cloud, Clouds, Environment, OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
+import * as THREE from "three";
+
+type SkyDiveProps = {
+  sentence: string | null;
+  flavor: Content.SkyDiveSliceDefaultPrimary["flavor"];
+};
+
+export default function Scene({ sentence, flavor }: SkyDiveProps) {
+  const groupRef = useRef<THREE.Group>(null);
+  const canRef = useRef<THREE.Group>(null);
+  const cloud1Ref = useRef<THREE.Group>(null);
+  const cloud2Ref = useRef<THREE.Group>(null);
+  const cloudsRef = useRef<THREE.Group>(null);
+  const wordsRef = useRef<THREE.Group>(null);
+
+  return (
+    <group>
+      {/* Can */}
+      <group rotation={[0, 0, 0.5]}>
+        <FloatingCan ref={canRef} flavor={flavor}></FloatingCan>
+      </group>
+
+      {/* Clouds */}
+      <Clouds ref={cloudsRef}>
+        <Cloud ref={cloud1Ref} bounds={[10, 10, 2]} />
+        <Cloud ref={cloud2Ref} bounds={[10, 10, 2]} />
+      </Clouds>
+
+      {/* Text */}
+
+      <OrbitControls />
+
+      {/* Lighting */}
+      <ambientLight intensity={2} color="#9DDEFA" />
+      <Environment files="/hdr/field.hdr" environmentIntensity={1.5} />
+    </group>
+  );
+}
